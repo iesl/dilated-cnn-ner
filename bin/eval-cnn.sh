97c289bf5b48eb11cdb2f454496329746bb8ffae
Emma Strubell <emma.strubell@gmail.com>
2017-05-07 21:50:42 -0400

    more fixing

--- bin/eval-cnn.sh
+++ bin/eval-cnn.sh
@@ -17,7 +17,7 @@ fi
 # star escaping
 dev_fixed=`echo "$dev_dir" | sed 's/\*/\\\*/'`
 
-cmd="$SIMPLE_CNN_NER_ROOT/bin/train-cnn.sh \
+cmd="$DILATED_CNN_NER_ROOT/bin/train-cnn.sh \
 $conf \
 --evaluate_only \
 --train_eval \
